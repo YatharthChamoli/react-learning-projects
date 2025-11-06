4f0dba2b4b0b54d1fea5e8d0c3c89ce98be4b312
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2025-11-07 00:53:48 +0530

    learing

--- dice-game/src/App.jsx
+++ dice-game/src/App.jsx
@@ -1,10 +1,20 @@
 import LuckyN from "./LuckyN"
+import { sum } from "./utils"
+
+function lessThan4(dice) {
+  return sum(dice) < 4;
+}
+
+function allSameValue(dice) {
+  return dice.every((v) => v === dice[0]);
+}
 
 function App() {
   return (
     <>
-      <LuckyN />
-      <LuckyN numDice={3} goal={11}/>
+      <LuckyN winCheck={lessThan4} title="Roll less than 4"/>
+      <LuckyN winCheck={allSameValue} numDice={3} title="Roll the same number"/>
+      
     </>
   )
 }
--- dice-game/src/LuckyN.jsx
+++ dice-game/src/LuckyN.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
-import { getRolls , sum } from './utils';
+import { getRolls } from './utils';
 import Dice from './Dice.jsx'; 
+import Button from './Button.jsx';
 
-function LuckyN({numDice = 2, goal=7 }) {
+function LuckyN({ title= "Dice Game", numDice = 2, winCheck }) {
   const [dice, setDice] = useState(getRolls(numDice));
-  const isWinner = sum(dice) === goal;
+  const isWinner = winCheck(dice);
   const roll = () => setDice(getRolls(numDice));
   return(
     <main className=""> 
-      <h1>Lucky {goal} {isWinner && "You win!"}</h1>
+      <h1>{title} {isWinner && "You win!"}</h1>
       <Dice dice={dice} />
-      <button onClick={roll}>Re-Roll Dice</button>
+      {/* <button onClick={roll}>Re-Roll Dice</button> */}
+      <Button clickFunc={roll} label="Re-roll"/>
     </main>
   )
 }
